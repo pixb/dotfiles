8fd4b2e95e3ced49091d3c1d47c0d4b6e07827cc
pix <tpxsky@163.com>
2026-09-22 14:02:33 +0800

    <fix>:fix fzf search config.

--- config/fzf.zsh
+++ config/fzf.zsh
@@ -1,6 +1,11 @@
-# fzf
-export FZF_DEFAULT_OPTS='--bind ctrl-e:down,ctrl-u:up,ctrl-d:preview-page-down,ctrl-f:preview-page-up --preview "[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (batcat --color=always {} || bat --color=always {} || ccat --color=always {} || highlight -O ansi -l {} || cat {}) 2> /dev/null | head -500" --preview-window right:60%:wrap --height 80% --layout reverse --border'
-export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
-export FZF_COMPLETION_TRIGGER='\'
-export FZF_TMUX_HEIGHT='80%'
-export FZF_PREVIEW_COMMAND='[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (ccat --color=always {} || highlight -O ansi -l {} || cat {}) 2> /dev/null | head -500'
+# fzf shell keybindings
+[ -f /usr/share/fzf/key-bindings.zsh ] && source /usr/share/fzf/key-bindings.zsh
+
+# Override Ctrl-T → Ctrl-F for file finder
+bindkey -M emacs '^F' fzf-file-widget
+bindkey -M vicmd '^F' fzf-file-widget
+bindkey -M viins '^F' fzf-file-widget
+
+export FZF_DEFAULT_OPTS='--bind ctrl-e:down,ctrl-u:up,ctrl-d:preview-page-down,ctrl-f:preview-page-up --preview "[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (batcat --color=always {} || bat --color=always {} || highlight -O ansi -l {} || cat {}) 2> /dev/null | head -500" --preview-window right:60%:wrap --height 80% --layout reverse --border'
+export FZF_DEFAULT_COMMAND='fd --hidden --ignore-file=.gitignore --exclude .git --type f'
+export FZF_PREVIEW_COMMAND='[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (batcat --color=always {} || bat --color=always {} || highlight -O ansi -l {} || cat {}) 2> /dev/null | head -500'
